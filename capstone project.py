637baccf2b31e04a70a88ca3fe8ba6b76073d949
Nupursaboo <63063294+Nupursaboo@users.noreply.github.com>
2020-07-30 12:22:04 +0300

    Add files via upload

---
+++ capstone project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone project
+
+# #### This notebook is going to be used for the capstone project which is a part of the IBM data science course
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone project course!")
+
+
+# In[ ]:
+
+
+
+
